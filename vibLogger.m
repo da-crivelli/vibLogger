be4b03635f50e4caeb38457c4a1573fcba08c340
Davide Crivelli <davide.crivelli@diamond.ac.uk>
2020-05-04 13:31:01 +0100

    move create_vib_config_file function inside vibLogger

--- vibLogger.m
+++ vibLogger.m
@@ -96,3 +96,26 @@ function display_data(t, data, settings)
         
     end
 end
+
+function create_vib_config_file(filename,settings)
+    %CREATE_VIB_CONFIG_FILE(filename,settings) creates a .m configuration file for vibration data
+
+    cfg = fopen(filename,'w');
+    fprintf(cfg,'%s\n','% Automatically generated config file');
+    fprintf(cfg,'%% Generated on: %s\n\n',datestr(now,'yyyy/mm/dd HH:MM:ss'));
+
+    sensors_string = sprintf('''%s'',',settings.sensorIDs{:});
+    fprintf(cfg,'sensor_ids = {%s};\n',sensors_string(1:end-1));
+
+    channel_string = sprintf('''%s'',',settings.channel_names{:});
+    fprintf(cfg,'channel_names = {%s};\n',channel_string(1:end-1));
+
+    if(isfield(settings,'transmiss_inputs'))
+        settings.transmiss_inputs(:)
+        fprintf(cfg,'inputs = [%s];\n',strtrim(sprintf('%.0f ',settings.transmiss_inputs(:))));
+        fprintf(cfg,'outputs = [%s];\n',strtrim(sprintf('%.0f ',settings.transmiss_outputs(:))));
+    end
+
+    fclose(cfg);
+
+end

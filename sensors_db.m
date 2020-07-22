84a9e1b44610a10586c3fddd318d070ee8b82196
Davide Crivelli <davide.crivelli@diamond.ac.uk>
2020-07-22 15:45:54 +0100

    add option to return sensors database as a variable

--- sensors_db.m
+++ sensors_db.m
@@ -7,6 +7,8 @@ function [sens] = sensors_db(in)
 %   SENSORS_DB('list') prints a list of all available sensors and serial
 %   numbers
 %
+%   SENSORS_DB('all') returns the sensor DB as a variable
+%
 %   Davide Crivelli
 %   davide.crivelli@diamond.ac.uk
 %
@@ -77,6 +79,8 @@ function [sens] = sensors_db(in)
                     fprintf('%s\t\t%s\t%.3d\t%s\n',sensors(k{1}).make, ...
                         sensors(k{1}).model,sensors(k{1}).sens,k{1});
                 end
+            case 'all'
+                sens = sensors;
             otherwise
                 error('Error. \nOption ''%s'' not recognised',in);
         end

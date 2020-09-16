b5a2747dc4a83f2fea87ca41a6efcbef45b63ef3
Davide Crivelli <davide.crivelli@diamond.ac.uk>
2020-09-16 15:01:56 +0100

    Add ability to specify multiple percentiles for probability distribution stats output

--- vibPlots.m
+++ vibPlots.m
@@ -104,6 +104,7 @@ end
 
 %% sets the "diary" file so that the output from some functions can be saved
 if(opts.SAVE_PLOTS)
+    diary on
     diary_file = [opts.fg_output_folder,'stats.txt'];
     if(exist(diary_file,'file'))
         delete(diary_file);

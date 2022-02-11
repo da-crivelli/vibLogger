4310a21f5e84b19d4786433b69dca156822582d8
Davide Crivelli <davide.crivelli@diamond.ac.uk>
2022-02-11 14:26:16 +0000

    add example for live transmissibility

--- Examples/runVibLogger.m
+++ Examples/runVibLogger.m
@@ -1,22 +1,24 @@
 clearvars
 close all
 
-addpath('\\dc.diamond.ac.uk\dls\science\users\mca67379\MATLAB\vibLogger');
+addpath('C:\Users\Public\MATLAB\viblogger');
 
 %% config
 
 settings = struct();
 
-settings.device_ids = {'cDAQ2Mod3','cDAQ2Mod4'};  % run "devices = daq.getDevices" and find the device ID
+settings.device_ids = {'SimDev1'};  % run "devices = daq.getDevices" and find the device ID
 
 settings.channels{1} = [0, 1, 2];  % which channels to read on each module / device
-settings.channels{2} = [0, 1, 2];  % which channels to read on each module / device
+%settings.channels{2} = [0, 1, 2];  % which channels to read on each module / device
 
-settings.sensorIDs = {'unity','unity','unity','58595','58594','50887'};  %sensor IDs (run sensors_db('list') to see all sensors)
-settings.channel_names = {'Mono X','Mono Y','Mono Z','Ground X','Ground Y','Ground Z'}; %channel names
+settings.sensorIDs = {'unity','unity','unity'};  %sensor IDs (run sensors_db('list') to see all sensors)
+settings.channel_names = {'1','2','3'}; %channel names
 
-%settings.transmiss_inputs = [1 2];  % only for transmissibility ratio tests.
-%settings.transmiss_outputs = [3 4]; % Comment out for standard vibration test
+settings.transmiss_inputs = [1 2 3];  % only for transmissibility ratio tests.
+settings.transmiss_outputs = [2 3 1]; % Comment out for standard vibration test
+settings.winoverlap = 0.67; % for live transmissibility
+settings.nrwindows = 2; % for live transmissibility
 
 settings.channel_type = 'Voltage';  % 'Voltage' for standard voltage
                                  % 'IEPE' for IEPE / ICP sensors.
@@ -26,10 +28,10 @@ settings.channel_type = 'Voltage';  % 'Voltage' for standard voltage
 %settings.iepe_excitation_current = .004;                         
                         
 settings.fsamp = 2048;   % sampling frequency in Hz
-settings.recording_time = 60; % time to record in seconds per block
+settings.recording_time = 2; % time to record in seconds per block
 settings.update_time = 1; %time in-between plots and other functions updates
 
-settings.timeout = 200;   % max acquisition time in seconds
+settings.timeout = 2;   % max acquisition time in seconds
 
 settings.output_folder = '.'; %where to save the results
 
@@ -37,7 +39,7 @@ settings.live_preview = true;
 settings.save_data = false;      % currently there is no way to 
                                 % independently set the save and preview times
 
-settings.callback_shortloop = 'transmissibility'
+settings.callback_shortloop = 'live_transmissibility';
                                 
 %% runs vibLogger                                
 vibLogger(settings);
